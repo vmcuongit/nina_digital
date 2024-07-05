4f900e0b144b6b606cb81caa837d06c2c4685023
vmcuongit <vmcuong1991@gmail.com>
2024-07-05 17:04:02 +0700

    update onboarding 5/7

--- lib/shared/constants/media_assets.dart
+++ lib/shared/constants/media_assets.dart
@@ -1,11 +1,5 @@
 class MediaAssets {
-  static const String assetsImages = 'assets/images';
-  static const String assetsOnboarding = 'assets/onboarding';
-  static const String assetsIcons = 'assets/icons';
-
-  static const String logoNina = '$assetsImages/logo_nina.png';
-  static const String logoNinaWhiteNoText =
-      '$assetsImages/logo_nina_white_notext.png';
-  static const String logoNinaWithText = '$assetsImages/logo.png';
-  static const String logoNinaWhite = '$assetsImages/logo_nina_white.png';
+  static const String images = 'assets/images';
+  static const String onboarding = 'assets/onboarding';
+  static const String icons = 'assets/icons';
 }
